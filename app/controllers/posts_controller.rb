2a0e844bb533300111c45640d1dc52f1c1032c21
yashka713 <liakhyaroslav@gmail.com>
2017-03-23 10:30:29 +0200

    step 4: error displaying

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,21 +20,19 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    authorize! :destroy, @post
+    authorize! :update, @post
   end
 
   # POST /posts
   # POST /posts.json
   def create
     @post = current_user.posts.new(post_params)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { redirect_to new_post_url, @post.errors }
       end
     end
   end
@@ -66,13 +64,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :content, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, :content, presence: true
+
   belongs_to :user
   has_many :comments
 end
