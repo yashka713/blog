5d2ba99dd4d8f9f98d322c18ba11f9e8f41d64a5
Yaroslav <liakhyaroslav@gmail.com>
2017-04-26 00:24:22 +0300

    try

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../boot', __FILE__)
+ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
+require 'rails/all'
+
+require_relative 'boot'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Blog
+  class Application < Rails::Application; end
+end
